b7713dcd7edd46ad09b75e105bb1cf50fd1113d3
Barry Yu <doung1118@gmail.com>
2020-07-15 18:16:46 +0800

    1

---
+++ main.js
@@ -0,0 +1,40 @@
+
+
+// example : hello 
+
+let str = 'hello'
+
+console.log(str.length)
+
+console.log(str[0])
+
+console.log(str[1])
+
+console.log(str[2])
+
+console.log(str[3])
+
+console.log(str[4])
+
+
+
+
+function returnWord(string) {
+
+  // point last word 
+
+
+  let string = ''
+
+  for (let i = string.length; str < i; i--) {
+
+
+
+
+  }
+
+
+
+  return returnWord
+
+}
